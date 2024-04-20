28a3c78ec73b2a857266f0e135e398d7ab10c1dc
Maker <Maker>
2024-04-20 11:52:40 +0200

    cleaned up code and comments

--- DHT11.c
+++ DHT11.c
@@ -1,6 +1,5 @@
 #include <string.h>
 #include <wiringPi.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include "DHT11.h"
 
@@ -20,7 +19,7 @@ int* readDHT(int dhtpin){
     pinMode(dhtpin, INPUT);//DHT pulls data pin to low
 
 
-    // delayMicroseconds(160);//DHT pulls to low for 80us and up for 80us - transmission start
+    //DHT pulls to low for 80us and up for 80us
     while(digitalRead(dhtpin) == LOW){
         delayMicroseconds(1);
     }
@@ -28,42 +27,35 @@ int* readDHT(int dhtpin){
     while(digitalRead(dhtpin) == HIGH){
         delayMicroseconds(1);
     }
-
+    //transmission start
     for(int i = 0; i < 40; i++){ //read 40 bits
         
         while(digitalRead(dhtpin) == LOW){//delay until high
             delayMicroseconds(1);
         }
-        //delayMicroseconds(50);
 
         int counter = 0;
         while(digitalRead(dhtpin) == HIGH){//count how long pin is high, 27 microseconds means 0, 70 microseconds means 1
             delayMicroseconds(1);
             counter++;
             if(counter > 80){
-                //printf("stuck on start\n");
                 return NULL; //something went horribly wrong, skip
             }
         }
-        //printf("%d\n", counter);
-
+    
         if(counter >= 30){//if higher than 30, assume its 1
             int bit = 1<<bit_position; //we gotta left-shift the bit by 7 decreasing because we are working with MSB first
-            //printf("BIT: %d\n", bit);
-            //printf("1");
             dht_data[byte_position] |= bit;
             bit_position--;
             
         }
         else{ //else its 0
             bit_position--;//0 we aint gotta do SHIT
-            //printf("0");
         }
 
         if(bit_position < 0){
             bit_position = 7;
             byte_position++;
-            //printf("\n");
         }
 
         int tempCount = 0;
@@ -71,7 +63,6 @@ int* readDHT(int dhtpin){
             delayMicroseconds(1);
             tempCount++;
             if(tempCount > 100){
-                //printf("stuck on end\n");
                 return NULL; //something went horribly wrong, skip
             }
         }
